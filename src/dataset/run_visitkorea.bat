0485c4387d421cb78f79d3acd30e857d4d7e39d8
Yunjeong Im <yunjeong.im@snu.ac.kr>
2025-12-01 16:13:24 +0900

    Update run_visitkorea.bat

--- src/dataset/run_visitkorea.bat
+++ src/dataset/run_visitkorea.bat
@@ -1,7 +1,7 @@
-python test.py --type ms --out ms_main --empty-patience 2
-python test.py --type food --out food_main --empty-patience 2
-python test.py --type acm --out acm_main --empty-patience 2
+python visitkorea_main.py --type ms --out ms_main
+python visitkorea_main.py --type food --out food_main
+python visitkorea_main.py --type acm --out acm_main
 
-python visitkorea_details.py --type ms --data ms_main_list.csv --out ms_details --limit 3
-python visitkorea_details.py --type food --data food_main_list.csv --out food_details --limit 3
-python visitkorea_details.py --type acm --data acm_main_list.csv --out acm_details --limit 3
+python visitkorea_details.py --type ms --data ms_main_list.csv --out ms_details
+python visitkorea_details.py --type food --data food_main_list.csv --out food_details
+python visitkorea_details.py --type acm --data acm_main_list.csv --out acm_details
